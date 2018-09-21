06961ee8f44d96fe49b8c1f6e92de0baacf343e7
Olson Dimanche <yashiro@Olsons-MacBook-Pro.local>
2018-09-21 15:37:29 -0400

    My version of counter counter

--- src/js/index.js
+++ src/js/index.js
@@ -9,48 +9,12 @@ import 'bootstrap';
 //include your index.scss file into the bundle
 import '../styles/index.scss';
 
+import Home from './component/Home';
+
 //render your react application
 
-function SimpleCounter (props){
-    return (
-        <div className="outerDiv">
-            <div className="calendar">
-                <i className="far fa-clock"></i>
-            </div>
-
-            <div className="seconds">
-                {props.seconds}
-            </div>
-            <div className="tenth">
-                {props.tenth}
-            </div>
-            <div className="hundredth">
-                {props.hundredth}
-            </div>
-            <div className="thousandth">
-                {props.thousandth}
-            </div>
-        </div>
-    );
-}
-
-SimpleCounter.propTypes = {
-    seconds: PropTypes.number,
-    tenth: PropTypes.number,
-    hundredth: PropTypes.number,
-    thousandth: PropTypes.number
-};
-
-let counter = 0;
-setInterval(function(){
-    const thous = Math.floor(counter/1000 %10);
-    const huns = Math.floor(counter/100 %10);
-    const tens = Math.floor(counter/10 %10);
-    const ones = Math.floor(counter/1 %10);
-    counter++;
 
     ReactDOM.render(
-        <SimpleCounter thousandth={ones} hundredth={tens} tenth={huns} seconds={thous} />,
+        <Home />,
         document.querySelector('#app')
     );
-}, 1000);
